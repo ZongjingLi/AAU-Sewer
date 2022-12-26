8f82dd53d5070961e26a7f2334dd6f76ca4a8750
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-12-26 10:42:12 +0800

    update

---
+++ validation.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nnn
+
+from model import *
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+
+if __name__ == "__main__":
+    print("start the validation process")
