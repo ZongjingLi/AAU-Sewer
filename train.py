d2e2f8ad6c32ba0920408b897e0228d0d060c14e
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-12-26 13:57:04 +0800

    update

--- train.py
+++ train.py
@@ -30,31 +30,35 @@ def train(model,dataset,config):
             optim.step()
             optim.zero_grad()
             history.append(loss.detach().numpy())
-        
-            #plt.plot(history)
-            #plt.pause(0.001)
-            #plt.cla()
-            
+
             torch.save(model,"point_net.ckpt")
         print("epoch: {} total_loss:{}".format(epoch,total_loss))
 
     return model
 
 def train_transfer(model,source,target,config):
-    optim = torch.optim.Adam(model.parameters(), config.lr)
-    
+    optim = torch.optim.Adam(model.parameters(), config.lr) 
+    dataloader = DataLoader(source,batch_size = config.batch_size)
+
     for epoch in range(config.epoch):
         total_loss = 0
-        itr = 0
+        for sample in tqdm(dataloader):
+            data = sample[0];label = sample[1]
+            data = torch.tensor(data).permute([0,2,1])
+            logsmx,_,_ = model(data)   
 
-        possible_index = list(range(len(source)))
-        while len(possible_index) != 0:
-            for sample in range(config.batch_size):
-                sample_loc = np.random.choice(possible_index)
-                possible_index.remove(sample_loc)
+            loss = 0
+            for i in range(label.shape[0]):
+                loss -= logsmx[i][label[i]]
+            total_loss += loss.detach().numpy()
 
-                itr += 1 # add one more iteration
+            
+            loss.backward()
+            optim.step()
+            optim.zero_grad()
 
-        print("epoch: {} itr:{} total_loss:{}".format(epoch,itr,total_loss))
+            torch.save(model,"point_net.ckpt")
+        print("epoch: {} total_loss:{}".format(epoch,total_loss))
+
 
 
