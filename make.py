df508920e59dd119b925da7951cd63f1ade97b36
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-09-06 07:18:01 +0800

    Update make.py

--- make.py
+++ make.py
@@ -24,17 +24,18 @@ if __name__ == "__main__":
     npt = 1024
     if TransferName == "coral":
       transfor = CORAL(scaling='standard') # [Transfer Model]
-      out_points = 1024
     if TransferName == "locit":
       transfor = LocIT()# [Transfer Model]
     if TransferName == "tca":
-      transfor = TCA(n_components = 1024 * 3 / 4)# [Transfer Model]
-      out_points = 1024 / 4
+      transfor = TCA(n_components = 100 * 3 )# [Transfer Model]
     outputs = transfor.fit_transfer(aau_syn_test.train_data.reshape(N,npt*3), aau_real_test.train_data.reshape(M,npt*3))
     Xs_trans, Xt_trans = outputs
-    aau_syn_test.train_data = torch.tensor(Xs_trans.reshape(N,1024,3)).float()
-    aau_real_test.train_data = torch.tensor(Xt_trans.reshape(M,1024,3)).float()
+    aau_syn_test.train_data = torch.tensor(Xs_trans.reshape(N,100,3)).float()
+    aau_real_test.train_data = torch.tensor(Xt_trans.reshape(M,100,3)).float()
     print(aau_syn_test.train_data.shape, aau_real_test.train_data.shape)
+
+    np.save("{}_syn_test.npy".format(TransferName),aau_syn_test.train_data)
+    np.save("{}_real_test.npy".format(TransferName),aau_real_test.train_data)
 
     # [Create Aligned Dataset] (Train)
     aau_syn_train =  AAUSewer("train","synthetic")
@@ -42,16 +43,13 @@ if __name__ == "__main__":
     N = aau_syn_train.train_data.shape[0]
     M = aau_real_train.train_data.shape[0]
     print("Transfer from {} to {}".format(N,M))
-    npt = 1024
-    
-    transfor = CORAL(scaling='standard') # [Transfer Model]
+
+    #transfor = CORAL(scaling='standard') # [Transfer Model]
     outputs = transfor.fit_transfer(aau_syn_train.train_data.reshape(N,npt*3), aau_real_train.train_data.reshape(M,npt*3))
     Xs_trans, Xt_trans = outputs
-    aau_syn_train.train_data = torch.tensor(Xs_trans.reshape(N,out_points,3)).float()
-    aau_real_train.train_data = torch.tensor(Xt_trans.reshape(M,out_points,3)).float()
+    aau_syn_train.train_data = torch.tensor(Xs_trans.reshape(N,100,3)).float()
+    aau_real_train.train_data = torch.tensor(Xt_trans.reshape(M,100,3)).float()
     print(aau_syn_train.train_data.shape, aau_real_train.train_data.shape)
 
-    np.save("{}_syn_test.npy".format(TransferName),aau_syn_test.train_data)
-    np.save("{}_syn_train.npy".format(TransferName),aau_syn_train.train_data)
-    np.save("{}_real_test.npy".format(TransferName),aau_real_test.train_data)
+    np.save("{}_syn_train.npy".format(TransferName),aau_real_test.train_data)
     np.save("{}_real_train.npy".format(TransferName),aau_real_train.train_data)
